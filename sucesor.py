e37d1115b783d20977c5fe7e57bd0bf3e9c7cb0b
Ignacio Ireta <72747576+ignacio-ireta@users.noreply.github.com>
2023-02-28 11:52:46 -0600

    Update sucesor.py
    Added support for latex format printing

--- sucesor.py
+++ sucesor.py
@@ -1,18 +1,17 @@
 n = int(input())
 
-def sucesor(N):
+
+def successor(number):
 
     rationals = []
     results = []
 
-    for i in range (1, N + 1):
-        rationals.append(f'1/10^{i}')
+    for i in range(1, number + 1):
+
+        rationals.append(f'\ufe68frac \u007b1\u007d \u007b10**{i}\u007d')
         results.append(1/(10**i))
 
     return rationals, results
 
-print(sucesor(n))
 
-
-
-
+print(successor(n))
